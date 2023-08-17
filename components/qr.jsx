dff6391edb1bc826e4fc4e1836b3da2959e67ded
Frank Cairampoma <ingfralch@gmail.com>
2023-08-17 11:54:57 -0500

    ya funciona la camara

--- components/qr.jsx
+++ components/qr.jsx
@@ -1,38 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { Camera, CameraType } from 'expo-camera';
+import { Camera, CameraType, camaraRef } from 'expo-camera';
 
 export default function Qr() {
     const navigation = useNavigation();
+    const [type, setType] = useState(CameraType.back);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+
+
+    if (!permission) {
+        return <View />;
+    }
+
+    if (!permission.granted) {
+        return (
+            <View style={styles.container}>
+                <Text style={{ textAlign: 'center' }}>Usted necesita brindar permisos para usar la camara</Text>
+                <Button onPress={requestPermission} title="grant permission" />
+            </View>
+        );
+    }
+
+    function toggleCameraType() {
+        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+      }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>QR Gym</Text>
-            {/* <Image style={styles.qr} source={require('../assets/qr.png')} /> */}
-            <Text style={{ color:"#aaa"}}>Escanear codigo QR </Text>
-            <View style={{flexDirection:"row"}}>
-                <Camera style={{width:200, height:200, margin:10}} type={CameraType.back} />
-                
-
-            </View>
+           <Camera style={styles.camera} type={type}>
+                <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+                    <Text style={styles.text}>Flip Camera</Text>
+                </TouchableOpacity>
+                </View>
+            </Camera>
         </View>
     );
 }
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#34495E',
-        alignItems: 'center',
-        justifyContent: 'center',
+      flex: 1,
+      justifyContent: 'center',
     },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginVertical: 10,
-        color: 'white',
+    camera: {
+      flex: 1,
     },
-    
-    
-});
+    buttonContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      backgroundColor: 'transparent',
+      margin: 64,
+    },
+    button: {
+      flex: 1,
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+    },
+    text: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+  });
