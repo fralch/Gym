388df3a0d54d96d31093ba0ce8e9c4aaaedcb4f7
Frank Cairampoma <ingfralch@gmail.com>
2023-08-18 18:31:51 -0500

    poniendo logo fb

--- components/qr.jsx
+++ components/qr.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, Image, TouchableOpacity, Modal, Dimensions   } from 'react-native';
+import { Text, View, StyleSheet, Button, Image, TouchableOpacity, Modal, Dimensions, Linking } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 const screenHeight = Dimensions.get('window').height;
@@ -38,33 +38,45 @@ export default function App() {
   return (
     <View style={styles.container}>
       <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} style={StyleSheet.absoluteFillObject}>
-      <View style={{marginTop:50, alignSelf: 'center'}}>
-                    <Image source={{ uri: 'https://media1.thehungryjpeg.com/thumbs2/ori_4208435_29eq7q29mxgwrrmkklgfz9uilwqvjgnozkapouif_gym-lion-esport-mascot-logo-design.png' }} style={{ width: 150, height: 90 }} />
-                </View>
+        <View style={{ marginTop: 50, alignSelf: 'center' }}>
+          <Image source={{ uri: 'https://media1.thehungryjpeg.com/thumbs2/ori_4208435_29eq7q29mxgwrrmkklgfz9uilwqvjgnozkapouif_gym-lion-esport-mascot-logo-design.png' }} style={{ width: 150, height: 90 }} />
+        </View>
       </BarCodeScanner>
-      {!scanned && 
-       <TouchableOpacity style={styles.button} onPress={() => toggleModal()}>
-             <Text style={{color:"white"}}>SCANEAR DE NUEVO</Text>
+      {!scanned &&
+        <TouchableOpacity style={styles.button} onPress={() => toggleModal()}>
+          <Text style={{ color: "white" }}>SCANEAR DE NUEVO</Text>
         </TouchableOpacity>
-     
+
       }
 
       <Modal visible={isModalVisible} animationType="slide">
         <View style={[styles.modalContainer, { height: screenHeight * 0.7 }]}>
           {/* Crear la interfas para mostrar los datos del usuario, Foto, nombre, Fecha */}
-  
-          <Image source={{ uri: 'https://scontent.fjau2-1.fna.fbcdn.net/v/t39.30808-6/340986589_259945029721074_1388666719680320276_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFY_6CqFfCRPaAY9FvFtybS_rUKpN1CnhT-tQqk3UKeFEkXayg6mLQLgzUeZoCRZgjEVb4utEXPUOg1GbvZB9bi&_nc_ohc=BVrDOCXNdUQAX9KfvF_&_nc_ht=scontent.fjau2-1.fna&oh=00_AfDcJS_9GjdBnEHKDQGpiV2-JLDzd1x4A8CUmDaTbdXsLA&oe=64E465C0' }} style={styles.fotoperfil} />
-          
-          <Text style={{color:"#aaa", fontSize:18, marginVertical:5}}>Nombre:<Text style={{color:"white", fontSize:18}}> Alexander Frank Cairampoma</Text> </Text>
-          <Text style={{color:"#aaa", fontSize:18, marginVertical:5}}>DNI: <Text style={{color:"white", fontSize:18}}> 12345678</Text></Text>
-          <Text style={{color:"#aaa", fontSize:18, marginVertical:5}}>Fecha Inicio: <Text style={{color:"white", fontSize:18}}> 19-09-2022</Text></Text>
-          <Text style={{color:"#aaa", fontSize:18, marginVertical:5}}>Fecha Fin: <Text style={{color:"white", fontSize:18}}>19-09-2023</Text></Text>
-          <Text style={{color:"#aaa", fontSize:18, marginVertical:5}}>Dias restantes: <Text style={{color:"white", fontSize:18}}>298</Text></Text>
-          <TouchableOpacity style={styles.button} onPress={() => toggleModal()}>
-              <Text style={{color:"white"}}>CERRAR</Text>
-          </TouchableOpacity>
-          
 
+          <Image source={{ uri: 'https://scontent.fjau2-1.fna.fbcdn.net/v/t39.30808-6/340986589_259945029721074_1388666719680320276_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFY_6CqFfCRPaAY9FvFtybS_rUKpN1CnhT-tQqk3UKeFEkXayg6mLQLgzUeZoCRZgjEVb4utEXPUOg1GbvZB9bi&_nc_ohc=BVrDOCXNdUQAX9KfvF_&_nc_ht=scontent.fjau2-1.fna&oh=00_AfDcJS_9GjdBnEHKDQGpiV2-JLDzd1x4A8CUmDaTbdXsLA&oe=64E465C0' }} style={styles.fotoperfil} />
+
+          <Text style={{ color: "#aaa", fontSize: 18, marginVertical: 5 }}>Nombre:<Text style={{ color: "white", fontSize: 18 }}> Alexander Frank Cairampoma</Text> </Text>
+          <Text style={{ color: "#aaa", fontSize: 18, marginVertical: 5 }}>DNI: <Text style={{ color: "white", fontSize: 18 }}> 12345678</Text></Text>
+          <Text style={{ color: "#aaa", fontSize: 18, marginVertical: 5 }}>Fecha Inicio: <Text style={{ color: "white", fontSize: 18 }}> 19-09-2022</Text></Text>
+          <Text style={{ color: "#aaa", fontSize: 18, marginVertical: 5 }}>Fecha Fin: <Text style={{ color: "white", fontSize: 18 }}>19-09-2023</Text></Text>
+          <Text style={{ color: "#aaa", fontSize: 18, marginVertical: 5 }}>Dias restantes: <Text style={{ color: "white", fontSize: 18 }}>298</Text></Text>
+          <TouchableOpacity style={styles.button} onPress={() => toggleModal()}>
+            <Text style={{ color: "white" }}>CERRAR</Text>
+          </TouchableOpacity>
+
+
+        </View>
+        <View style={styles.footer}>
+          <TouchableOpacity
+            style={styles.socialButton}
+            onPress={() => Linking.openURL('https://www.facebook.com/frank.cairampoma.castro')}
+          >
+            <Image
+              source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/2048px-2021_Facebook_icon.svg.png' }}
+              style={{ width: 50, height: 50 }}
+            />
+            <Text style={styles.socialButtonText}>Facebook</Text>
+          </TouchableOpacity>
         </View>
       </Modal>
     </View>
@@ -76,7 +88,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-end',
-    alignSelf: 'stretch', 
+    alignSelf: 'stretch',
     backgroundColor: "#34495E"
   },
   button: {
@@ -100,11 +112,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#34495E"
   },
   fotoperfil: {
-    width: 150, 
-    height: 150, 
-    marginVertical:20, 
+    width: 150,
+    height: 150,
+    marginVertical: 20,
     borderRadius: 100,
     borderWidth: 2,
     borderColor: "white"
-  }
+  },
+  footer: {
+    height: screenHeight * 0.1,
+    backgroundColor: '#34495E',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  socialButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  socialButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
 });
